36f96d4da1e05b492f7c3bf272b0086821884d4b
user <1552113505@qq.com>
2023-12-20 15:49:18 +0000

    “âfix nan issues

--- evaluation/reranker_eval.py
+++ evaluation/reranker_eval.py
@@ -1,14 +1,31 @@
 # -*- coding: utf-8 -*-
+import torch
 import ir_datasets
 import configparser
 import pyterrier as pt
 from ir_measures import nDCG, AP
+from transformers import AutoTokenizer, LlamaForCausalLM, LlamaTokenizer, BitsAndBytesConfig
 
 from utils.data_utils import construct_topics, construct_qrels
-from src.reranker.llama_reranker import LlamaReranker, LlamaRerankerWrapper
+from src.reranker.llama_reranker import LlamaReranker
 
 if not pt.started():
     pt.init()
+
+use_4bit = True
+bnb_4bit_compute_dtype = "float32"
+bnb_4bit_quant_type = "nf4"
+compute_dtype = getattr(torch, bnb_4bit_compute_dtype)
+use_nested_quant = False
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=use_4bit,
+    bnb_4bit_quant_type=bnb_4bit_quant_type,
+    bnb_4bit_compute_dtype=compute_dtype,
+    bnb_4bit_use_double_quant=use_nested_quant
+)
+
+torch.cuda.empty_cache()
 
 
 def evaluation(conf_file = None):
@@ -30,12 +47,18 @@ def evaluation(conf_file = None):
     topics = construct_topics(topic_file_path)
     qrels = construct_qrels(qrels_file_path)
     
-    llama_reranker = LlamaReranker()
-    llama_reranker_wrapper = LlamaRerankerWrapper(llama_reranker)
+    #llama_reranker = LlamaReranker()
+    #llama_reranker_wrapper = LlamaRerankerWrapper(llama_reranker)
+
+    tokenizer = LlamaTokenizer.from_pretrained('/llm/llama2/llama2_7b', padding_side = "left")
+    tokenizer.add_special_tokens({"pad_token": "[PAD]"})
+    model = LlamaForCausalLM.from_pretrained('/llm/llama2/llama2_7b', torch_dtype=torch.float16, device_map={"": 0})
+    llama_reranker = LlamaReranker(model=model, 
+                                    tokenizer=tokenizer)
 
     text_ref = pt.get_dataset('irds:msmarco-passage')
     bm25 = pt.BatchRetrieve.from_dataset("msmarco_passage", "terrier_stemmed", wmodel="BM25")
-    pipe = bm25 >> pt.text.get_text(text_ref, 'text') >> llama_reranker_wrapper
+    pipe = (bm25 % 100) >> pt.text.get_text(text_ref, 'text') >> llama_reranker
     result = pt.Experiment(
         [pipe],
         topics,
--- src/reranker/llama_reranker.py
+++ src/reranker/llama_reranker.py
@@ -9,7 +9,7 @@ from typing import *
 from tqdm import tqdm
 from torch.nn.functional import softmax
 from vllm import LLM, SamplingParams
-from transformers import AutoTokenizer, LlamaForCausalLM, LlamaTokenizer
+from transformers import AutoTokenizer, LlamaForCausalLM, LlamaTokenizer, BitsAndBytesConfig
 from collections import defaultdict
 from src.prompt.prompt_manager import PairwisePrompt, PromptManager
 from pyterrier import Transformer
@@ -20,17 +20,31 @@ if not pt.started():
     pt.init()
 
 
-class LlamaReranker(object):
-    def __init__(self, model="/llm/llama2/llama2_7b", temperature=0.):
-        self.batch_size = 8
-        self.tokenizer = LlamaTokenizer.from_pretrained(model, padding_side = "left")
-        self.tokenizer.pad_token = self.tokenizer.eos_token
-        self.vocab_size = len(self.tokenizer.get_vocab().keys())
-        self.device = torch.device("cuda" if torch.cuda.is_available else "cpu")
-        self.device = "cpu"
-        self.model = LlamaForCausalLM.from_pretrained(model).to(self.device)
-        # self.llm = LLM(model=model)
-        # self.sampling_params = SamplingParams(temperature=temperature, logprobs=vocab_size)
+use_8bit = True
+bnb_8bit_compute_dtype = "float16"
+bnb_8bit_quant_type = "nf8"
+compute_dtype = getattr(torch, bnb_8bit_compute_dtype)
+use_nested_quant = False
+
+bnb_config = BitsAndBytesConfig(
+    load_in_8bit=use_8bit,
+    bnb_8bit_quant_type=bnb_8bit_quant_type,
+    bnb_8bit_compute_dtype=compute_dtype,
+    bnb_8bit_use_double_quant=use_nested_quant
+)
+#torch.cuda.clear_cache()
+torch.cuda.empty_cache()
+
+
+class LlamaReranker(Transformer):
+    def __init__(self, model, tokenizer, batch_size : int = 1):
+        super().__init__()
+        self.model = model
+        self.tokenizer = tokenizer
+        self.tokenizer.pad_token_id = 32000 # This stops nans for some reason, I trust github issues
+        self.TRUE = self.tokenizer.encode("true")[1] # lower case to suit the prompt
+        self.FALSE = self.tokenizer.encode("false")[1] # ^^
+        self.batch_size = batch_size
 
     def split_data_by_batch(self, prompts: List[str]):
         batch_data_list = []
@@ -38,53 +52,27 @@ class LlamaReranker(object):
             batch_data_list.append(prompts[idx: idx+self.batch_size])
 
         return batch_data_list
-        
-    def __call__(self, prompts: List[str], documents: List[str] = None):
-        reranker_results = []
 
-        true_token = self.tokenizer.encode("True")[1]
-        false_token = self.tokenizer.encode("False")[1]
-
-        batch_data_list = self.split_data_by_batch(prompts)
-
-        print("start tokenizer...")
-        inputs = []
-        for batch_data in tqdm(batch_data_list):
-            inputs.append(self.tokenizer(batch_data, padding="longest", return_tensors="pt").to(self.device))
-
+    def score(self, prompts: List[str], documents: List[str] = None):
         print("start model predict...")
         outputs = []
         with torch.no_grad():
-            for input in tqdm(inputs):
-                outputs.extend(self.model.forward(**input))
+            for batch in tqdm(self.split_data_by_batch(prompts)): # Save a few LOC just directly iterating
+                toks = self.tokenizer(batch, padding="longest", return_tensors="pt", add_special_tokens=True).to(self.model.device) # Same as before
+                logits = self.model.forward(**toks).logits
+                logits_after_softmax = logits[:, 0, (self.TRUE, self.FALSE)].log_softmax(dim=1) # Log softmax is generally used, can't remember why
+                print(logits_after_softmax[:, 0].cpu().detach().tolist())
+                outputs.extend(logits_after_softmax[:, 0].cpu().detach().tolist()) # Get just the softmax prob of 'true'
 
-        logits_after_softmax = outputs.logits[:, 0, (true_token, false_token)].softmax(dim=1)
-        logits_list = logits_after_softmax.cpu().detach().tolist()
-
-        scores = [item[0] for item in logits_list]
-        sorted_results = sorted(range(len(logits_list)), key=lambda x: logits_list[x][0], reverse=True)
-
-        if documents is not None:
-            for sorted_result in sorted_results:
-                reranker_results.append(documents[sorted_result])
-
-        return scores, reranker_results
-        
-
-class LlamaRerankerWrapper(Transformer):
-    def __init__(self, llama_reranker: LlamaReranker):
-        super().__init__()
-        self._llama_reranker = llama_reranker
+        return outputs
     
     def transform(self, topics_or_res: pd.DataFrame) -> pd.DataFrame:
-        prompts = topics_or_res.apply(lambda x: PairwisePrompt(query=x.query, documents=[x.text]).generate()[0], axis=1)
-        # print(prompts.to_dict())
+        prompts = topics_or_res.apply(lambda x : PairwisePrompt(query=x.query, documents=[x.text]).generate()[0], axis=1) # Point wise prompt creation
         prompts = list(prompts.to_dict().values())
-        scores, _ = self._llama_reranker(prompts)
-        topics_or_res["score"] = scores
-
+        scores = self.score(prompts)
+        topics_or_res["score"] = scores # ordered input therefore can assign list directly
         return add_ranks(topics_or_res)
-      
+
 
 if __name__== "__main__" :
     query = "cost of interior concrete flooring"
@@ -93,9 +81,15 @@ if __name__== "__main__" :
     # documents = [documents[0]]
     pairwise_prompt = PairwisePrompt(query=query, documents=documents)
     prompts = pairwise_prompt.generate()
-    print(prompts)
+    #print(prompts)
 
-    llama_reranker = LlamaReranker()
-    results = llama_reranker(documents, prompts)
+    documents = ["Time: 02:53. Video on the cost of concrete floors. The cost of a concrete floor is economical, about $2 to $6 per square foot depending on the level of complexity.", "Polished Concrete Prices. Polished concrete pricing and cost per square metre for concrete polishing can range from as little as Â£20 to as much as Â£150 per metre square. Additionally polished concrete overlays with exotic aggregates could add further costs of Â£10 to Â£50 per metre.olished concrete pricing and cost per square metre for concrete polishing can range from as little as Â£20 to as much as Â£150 per metre square.", "Stained Concrete Cost. With staining, itâs often possible to dress up plain gray concrete for less than the cost of covering it up with carpeting, tile, or most types of high-end flooring materials. At this price point, you are comparing against wood flooring ($8-$10 per square foot) and a range of ceramic and quarry tiles ($10-$12 per square foot). 2  Adding decorative sandblasting or engraving to the advanced stain application ($15+ per square foot)."]
+    frame = pd.DataFrame({'docno' : [*range(len(documents))], 'text': documents})
+    frame['qid'] = 1
+    frame['query'] = "cost of interior concrete flooring"
+    tokenizer = LlamaTokenizer.from_pretrained('/llm/llama2/llama2_7b', padding_side = "left")
+    llama_reranker = LlamaReranker(model=LlamaForCausalLM.from_pretrained('/llm/llama2/llama2_7b', quantization_config=bnb_config, device_map="auto"), 
+                                    tokenizer=tokenizer)
+    results = llama_reranker(frame)
     # print("\n".join(results))
     print(results)
